1fb30cd6cb66d580ecf37a0d30e8fef1f660601d
PF82 <pedrotcfernandes@gmail.com>
2022-07-23 00:57:44 +0100

    06-10

--- exercises-course-training/08.js
+++ exercises-course-training/08.js
@@ -12,11 +12,105 @@ The character and its pair are paired up in an array, and all the arrays are gro
 */
 
 function pairElement(str) {
-  return str;
+  // Function to match each character with the base pair
+  let matchWithBasePair = function (char, pairedArray) {
+    switch (char) {
+      case "A":
+        pairedArray.push(["A", "T"]);
+        break;
+      case "T":
+        pairedArray.push(["T", "A"]);
+        break;
+      case "C":
+        pairedArray.push(["C", "G"]);
+        break;
+      case "G":
+        pairedArray.push(["G", "C"]);
+        break;
+    }
+  };
+
+  // Find pair for every character in the string
+  const paired = [];
+  for (let i = 0; i < str.length; i++) {
+    matchWithBasePair(str[i], paired);
+  }
+
+  return paired;
 }
 
 pairElement("GCG");
 
 
-console.log()
+console.log(pairElement("GCG"))
 // document.getElementById("demo").innerHTML = "";
+
+/*
+Problem Explanation
+You will get a DNA strand sequence and you need to get the pair and return it as a 2D array of the base pairs. Keep in mind that the provided 
+strand should be first always.
+
+Another way to interpret the problem: there are four potential characters that exist in DNA: “A”, “T”, “G”, and “C”. “A” and “T” are always 
+paired together, and “G” and “C” are always paired together.
+This problem presents you with an input, e.g. “ATCGA”. Each of those five characters are missing their pairs.
+E.g. the first character “A” needs to be paired with “T” to give the array element [“A”, “T”].
+The second character “T” needs to be paired with “A” to give the array element [“T”, “A”].
+The number of elements in the final output equals the number of characters in the input.
+
+This problem does not involve rearranging the input into different combinations or permutations.
+
+Relevant Links
+Array.push() 
+String.split() 
+
+Hints
+Hint 1
+There are two base case, A-T and C-G, these go both way. You can use regular expression, if statements of anything that you can think of.
+Hint 2
+I would recommend using a switch, as it makes things a lot smoother.
+Hint 3
+The result must be an array of arrays, so keep that in mind when pushing things.
+
+Solution
+function pairElement(str) {
+  // Function to match each character with the base pair
+  let matchWithBasePair = function(char, pairedArray) {
+    switch (char) {
+      case "A":
+        pairedArray.push(["A", "T"]);
+        break;
+      case "T":
+        pairedArray.push(["T", "A"]);
+        break;
+      case "C":
+        pairedArray.push(["C", "G"]);
+        break;
+      case "G":
+        pairedArray.push(["G", "C"]);
+        break;
+    }
+  };
+
+  // Find pair for every character in the string
+  const paired = [];
+  for (let i = 0; i < str.length; i++) {
+    matchWithBasePair(str[i], paired);
+  }
+
+  return paired;
+}
+
+// test here
+pairElement("GCG")
+
+Code Explanation
+The program is very simple, the best solution that I have come up with is to use a switch to catch all the possible four elements. Using 
+if statements would take too much code. You could also use Regular Expressions.
+
+Since we have to the original and the pair, I decided to take all four cases instead of the base two.
+
+Create an empty array and use the search function to push the right values to the array and return them.
+
+Relevant Links
+Switch Statements 
+*/
--- exercises-course-training/09.js
+++ exercises-course-training/09.js
@@ -6,11 +6,77 @@ If all letters are present in the range, return undefined.
 */
 
 function fearNotLetter(str) {
-  return str;
+  for (var i = 0; i < str.length; i++) {
+    /* code of current character */
+    var code = str.charCodeAt(i);
+    /* if code of current character is not equal to first character + no of iteration
+        hence character has been escaped */
+    if (code !== str.charCodeAt(0) + i) {
+      /* if current character has escaped one character find previous char and return */
+      return String.fromCharCode(code - 1);
+    }
+  }
+  return undefined;
 }
 
 fearNotLetter("abce");
 
 
-console.log()
+console.log(fearNotLetter("abce"))
 // document.getElementById("demo").innerHTML = "";
+
+/*
+Problem Explanation
+You will create a program that will find the missing letter from a string and return it. If there is no missing letter, the program should 
+return undefined. There is currently no test case for the string missing more than one letter, but if there was one, recursion would be used. 
+Also, the letters are always provided in order so there is no need to sort them.
+
+Relevant Links
+String global object 
+JS String Prototype CharCodeAt 
+String.fromCharCode
+
+Hints
+Hint 1
+You will need to convert from character to ASCII code using the two methods provided in the description.
+
+Hint 2
+You will have to check for the difference in ASCII code as they are in order. Using a chart would be very helpful.
+
+Hint 3
+You will need to figure out where the missing letter is, along with handling the case that there is not missing letter as it needs an specific 
+return value.
+*/
+
+//Solution
+//function fearNotLetter(str) {
+  //for (var i = 0; i < str.length; i++) {
+    /* code of current character */
+    //var code = str.charCodeAt(i);
+
+    /* if code of current character is not equal to first character + no of iteration
+        hence character has been escaped */
+    //if (code !== str.charCodeAt(0) + i) {
+      /* if current character has escaped one character find previous char and return */
+      //return String.fromCharCode(code - 1);
+    //}
+  //}
+  //return undefined;
+//}
+
+// test here
+//fearNotLetter("abce");
+
+/*
+Code Explanation
+This solutions makes use of a for loop.
+Code of encountered character is stored in code.
+It is checked if code of current character is the expected one(no characters are skipped) by using the logic - code of current 
+character = code of first character + number of iterations.
+If a character is missing, the missing character is found and the final string is returned.
+undefined is returned if there is no missing character in the string.
+
+Relevant Links
+JS For Loops Explained 
+String.length
+*/
