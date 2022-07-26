cbc257e7ddbf6f052d5daa2da9545ab4cc26e2be
PF82 <pedrotcfernandes@gmail.com>
2022-07-26 19:45:00 +0100

    16-20

--- exercises-course-training/19.js
+++ exercises-course-training/19.js
@@ -15,11 +15,55 @@ If either argument isn't a valid number, return undefined.
 */
 
 function addTogether() {
-    return false;
+    const [first, second] = arguments;
+    if (typeof (first) !== "number")
+        return undefined;
+    if (second === undefined)
+        return (second) => addTogether(first, second);
+    if (typeof (second) !== "number")
+        return undefined;
+    return first + second;
 }
 
 addTogether(2, 3);
 
 
-console.log()
+console.log(addTogether(2, 3))
 // document.getElementById("demo").innerHTML = "";
+
+/*
+Problem Explanation
+It can be quite complicated to understand what needs to be done. There are always many ways to do something when coding but regardless of 
+the algorithm used, we have to create a program that does the following:
+
+It has to add two numbers passed as parameters and return the sum.
+It has to check if any of the numbers are actual numbers, otherwise return undefined and stop the program right there.
+It has to check if it has one or two arguments passed. More are ignored.
+If it has only one argument then it has to return a function that uses that number and expects another one, to then add it.
+
+Relevant Links
+Typeof 
+Arguments Object 
+
+Hints
+Hint 1
+Every time you deal with an argument, you have to check if it is defined and if it is a number.
+
+Hint 2
+When working on the case that it needs to return a function, using closure can help you write the new function in terms of addTogether().
+
+Hint 3
+The early return pattern can simplify your code.
+
+Solution
+function addTogether() {
+  const [first, second] = arguments;
+  if (typeof(first) !== "number")
+    return undefined;
+  if (second === undefined)
+    return (second) => addTogether(first, second);
+  if (typeof(second) !== "number")
+    return undefined;
+  return first + second;
+}
+*/
